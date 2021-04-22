b6c62e9f8bfe21c934c1e7413518e6b385ff80a6
Gabriel Cifuentes <1795@holbertonschool.com>
2021-04-22 14:25:12 +0000

    fix 0-lockboxes.py - new edge cases added

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,6 +4,10 @@
 
 def canUnlockAll(boxes):
     '''determines if all the boxes can be opened'''
+    if type(boxes) is not list or len(boxes) == 0:
+        return False
+    if len(boxes) == 1:
+        return True
     remaining = [x for x in range(1, len(boxes))]
     for key in boxes[0]:
         recursion(boxes, remaining, key)
