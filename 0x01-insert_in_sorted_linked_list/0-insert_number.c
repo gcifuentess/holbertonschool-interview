b2ba627fa3a01cc3094e9889dbe1050c21326fd0
Gabriel Cifuentes <1795@holbertonschool.com>
2021-04-29 21:05:37 +0000

    Fix 0-insert_number.c - edge case missing

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -15,22 +15,26 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *current;
 
 	current = *head;
-
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 
 	new->n = number;
-
+	new->next = NULL;
 	if (*head == NULL)
 		*head = new;
 	else
 	{
-		while (current->next != NULL && current->next->n < number)
+		while (current->next != NULL && current->n < number)
 			current = current->next;
 		new->next = current->next;
+		if (current->next)
+		{
+			new->n = current->n;
+			current->n = number;
+		}
+
 		current->next = new;
 	}
-
 	return (new);
 }
