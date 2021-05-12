4f8afb278c4096a28f647b11ea65cab91e83118e
Gabriel Cifuentes <1795@holbertonschool.com>
2021-05-12 23:45:15 +0000

    fix 0-minoperations.py - important bug fixed

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -15,11 +15,16 @@ def minOperations(n):
     copy = string
     oper = 1
     opt_div = optimal_divisor(n)
+    pos = len(opt_div) - 1
     while len(string) < n:
-        if oper < opt_div:
+        if oper < opt_div[pos]:
             string = string + copy
-        elif len(copy) == opt_div:
+        elif len(copy) == opt_div[pos]:
             string = string + copy
+            if len(string) == opt_div[pos - 1]:
+                pos -= 1
+                copy = string
+                oper += 1
         else:
             copy = string
         oper += 1
@@ -29,10 +34,12 @@ def minOperations(n):
 def optimal_divisor(n):
     '''Finds out optimal divisor of a positive integer'''
     i = n - 1
+    divisors = [n + 1]
     while i > 1:
         if n % i == 0:
             n = i
             i = n - 1
+            divisors.append(n)
         else:
             i -= 1
-    return n
+    return divisors
