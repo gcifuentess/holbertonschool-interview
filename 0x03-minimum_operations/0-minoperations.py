4da6d756e1cdf5d9aa2bb33b5498398eb52973aa
Gabriel Cifuentes <1795@holbertonschool.com>
2021-05-12 21:26:02 +0000

    add 0-minoperations.py 0-main.py

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+'''Min operations module'''
+
+
+def minOperations(n):
+    '''calculates the fewest number of operations needed to result in exactly
+    n H characters in the file
+    Args:
+        n: any int number
+    Return: Returns an integer, if n is impossible to achieve, return 0
+    '''
+    if type(n) is not int or n < 1:
+        return 0
+    string = "H"
+    copy = string
+    oper = 1
+    opt_div = optimal_divisor(n)
+    while len(string) < n:
+        if oper < opt_div:
+            string = string + copy
+        elif len(copy) == opt_div:
+            string = string + copy
+        else:
+            copy = string
+        oper += 1
+    return oper
+
+
+def optimal_divisor(n):
+    '''Finds out optimal divisor of a positive integer'''
+    i = n - 1
+    while i > 1:
+        if n % i == 0:
+            n = i
+            i = n - 1
+        else:
+            i -= 1
+    return n
