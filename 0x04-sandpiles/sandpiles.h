a8c50bc790e740ebc4dc243766855f1eea1a6dc4
Gabriel Cifuentes <1795@holbertonschool.com>
2021-05-23 17:49:22 +0000

    update 0-sandpiles sandpiles.h - total refactor

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -5,7 +5,8 @@
 #include <stdlib.h>
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-static void print_grid(int grid[3][3]);
 void stabilize(int grid[3][3]);
+char check_stability(int grid[3][3]);
+static void print_grid(int grid[3][3]);
 
 #endif /* SANDPILES_H */
