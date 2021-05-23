20f6eb89f94b2b352a02a9dd658918f654cd809e
Gabriel Cifuentes <1795@holbertonschool.com>
2021-05-23 18:10:14 +0000

    fix 0-sandpiles - bug fix in sandpiles_sum function

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -17,17 +17,17 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 		for (j = 0; j < 3; j++)
 		{
 			grid1[i][j] = grid1[i][j] + grid2[i][j];
-			if (grid1[i][j] > 3)
+			if (grid1[i][j] > 3 && check == 's')
 				check = 'u';
 		}
 	}
-	if (check == 'u')
+	while (check == 'u')
 	{
 		printf("=\n");
 		print_grid(grid1);
+		stabilize(grid1);
+		check = check_stability(grid1);
 	}
-
-	stabilize(grid1);
 }
 
 /**
@@ -38,41 +38,55 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
  */
 void stabilize(int grid[3][3])
 {
-	char check = 'u';
 	int i, j;
+	int tgrid[3][3] = {
+		{-1, -1, -1},
+		{-1, -1, -1},
+		{-1, -1, -1}
+	};
 
-	while (check == 'u')
+	/* copy the grid */
+	for (i = 0; i < 3; i++)
 	{
-		for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			tgrid[i][j] = grid[i][j];
+	}
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
 		{
-			for (j = 0; j < 3; j++)
+			if (tgrid[i][j] > 3)
 			{
-				if (grid[i][j] > 3)
-				{
-					grid[i][j] -= 4;
-					if (j - 1 > -1)
-						grid[i][j - 1] += 1;
-					if (j + 1 < 3)
-						grid[i][j + 1] += 1;
-					if (i - 1 > -1)
-						grid[i - 1][j] += 1;
-					if (i + 1 < 3)
-						grid[i + 1][j] += 1;
-				}
-				if (grid[i][j] <= 3)
-					check = 's';
-				else
-					check = 'u';
+				grid[i][j] -= 4;
+				if (j - 1 >= 0)
+					grid[i][j - 1] += 1;
+				if (j + 1 <= 2)
+					grid[i][j + 1] += 1;
+				if (i - 1 >= 0)
+					grid[i - 1][j] += 1;
+				if (i + 1 <= 2)
+					grid[i + 1][j] += 1;
 			}
 		}
-		if (check == 'u')
-		{
-			printf("=\n");
-			print_grid(grid);
-		}
-
 	}
-	printf("\n%c", check);
+}
+
+
+/**
+ * check_stability - checks if every number of the grid is <= 3
+ * @grid: a sandpile
+ *
+ * Return: 's' if stable or 'u' if not
+ */
+char check_stability(int grid[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			if (grid[i][j] > 3)
+				return('u');
+	return ('s');
 }
 
 
