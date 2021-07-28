77f3d78d35cddc3681ab30b2c9d1f8ee123fd9b1
Gabriel Cifuentes <1795@holbertonschool.com>
2021-07-28 05:58:02 +0000

    update binary_trees.h - typedef for binary_tree_t

--- 0x0D-sorted_array_to_avl/binary_trees.h
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
+  #include <stddef.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -13,13 +13,16 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} avl_t;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s avl_t;
 
 void binary_tree_print(const avl_t *);
 avl_t *sorted_array_to_avl(int *array, size_t size);
