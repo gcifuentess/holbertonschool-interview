fc9743639abd72d47ae2cb8b5cc21247c0fca6cd
Gabriel Cifuentes <1795@holbertonschool.com>
2021-12-12 16:05:14 +0000

    fix add 0-count.py - several changes - better recursion control

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -13,7 +13,7 @@ def count_words(subreddit, word_list, hot_list=[]):
     # checks if there is a new page "after" in the ctl_dict
     # in hot_list, the program stores temporary the clt_dict in the last index
     try:
-        my_params['after'] = hot_list[-1]['after']
+        my_params['after'] = hot_list[0]['after']
     except:
         pass
     r = requests.get(url, headers=my_header,
@@ -23,34 +23,32 @@ def count_words(subreddit, word_list, hot_list=[]):
     r = r.json()
     data = r['data']
     try:  # reads the ctl_dict, wich controls the recursion
-        if type(hot_list[-1]) is dict:
-            ctl_dict = hot_list[-1].copy()
-            del hot_list[-1]  # deleltes de ctl_dict from the list
+        if type(hot_list[0]) is dict:
+            ctl_dict = hot_list[0]
     except:
         ctl_dict = {'counter': 0, 'after': None}  # creates de ctl_dict
+        hot_list.append(ctl_dict)
     posts = data['children']
     nb_posts = len(posts)
     counter = ctl_dict['counter']
-    if counter < nb_posts:  # append all the posts of the page
-        hot_list.append(posts[counter]['data']['title'])
+
+    for nb_post in range(nb_posts):  # append all the posts of the page
+        hot_list.append(posts[nb_post]['data']['title'])
+    else:  # to start checking in the new page
         ctl_dict['counter'] = counter + 1
-        hot_list.append(ctl_dict)
-        count_words(subreddit, word_list, hot_list)
-    else:  # to start checking in the new page and free memory
-        ctl_dict['counter'] = 0
         ctl_dict['after'] = data.get('after', None)  # stores the next page
-        hot_list.append(ctl_dict)
-    if ctl_dict['counter'] == 1:  # Checks if we are in the first called func
-        if hot_list[-1]['after']:  # Checks if there is a new page
+        if hot_list[0]['after']:  # Checks if there is a new page
             count_words(subreddit, word_list, hot_list)
-        del hot_list[-1]
+
+    if counter == 1:  # Checks if we are in the first called func
         word_list_l = [x.lower() for x in word_list]
         dict_count = {x: 0 for x in word_list_l}
-        for title in hot_list:
+
+        for i in range(1, len(hot_list)):
+            title = hot_list[i]
             for word in word_list_l:
                 dict_count[word] = (dict_count[word] +
                                     title.lower().count(' ' + word + ' '))
         for k in sorted(dict_count, key=dict_count.get, reverse=True):
             if dict_count[k]:
                 print("{}: {}".format(k, dict_count[k]))
-    return hot_list
