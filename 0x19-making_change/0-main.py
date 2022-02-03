1b7002722d8b11b3fb7592124670f32b7dfdb360
Gabriel Cifuentes <1795@holbertonschool.com>
2022-02-03 02:13:43 +0000

    add 0-main.py 0-making_change.py

---
+++ 0x19-making_change/0-main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Main file for testing
+"""
+
+makeChange = __import__('0-making_change').makeChange
+
+print(makeChange([1, 2, 25], 37))
+
+print(makeChange([1256, 54, 48, 16, 102], 1453))
+
+print(makeChange([5, 10, 12, 2, 13, 11, 6, 1, 2, 8], 8))
+
+print(makeChange([991, 993, 997, 999, 998, 996, 995, 1, 994, 992],
+                 1000))
